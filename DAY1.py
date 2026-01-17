514102cdea3616eeab10de070a59923705dc89d9
Pranish Limbu <pranishlimbo@gmail.com>
2026-01-17 21:32:47 +0545

    Assignment 3

--- DAY1.py
+++
@@ -1,7 +0,0 @@
-Sci=float(input("Enter marks received in Science="))
-Math=float(input("Enter marks recieved in Maths="))
-Eng=float(input("Enter marks recieved in Eng="))
-
-Avg=(Sci+Math+Eng)/3
-print(Avg)
-
